e1adf224693ffc2d3374920cfc5abc8a2d0afeeb
Brice Bentler <server4001@gmail.com>
2017-04-12 01:19:09 -0700

    Bumps Ansible version from 1.9.6 to 2.2.1.0

--- provision.sh
+++ provision.sh
@@ -2,4 +2,4 @@
 
 # Install Ansible.
 sudo rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-sudo yum install -y ansible1.9 libselinux-python
+sudo yum install -y ansible libselinux-python
